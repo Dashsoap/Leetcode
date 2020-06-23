a9456ee3ce6c1e3c798b2238b932aa10abf3f8f8
unknown <zhoujingtian@canaan-creative.com>
2020-06-23 15:45:29 +0800

    67题AC

--- 67.二进制求和.js
+++ 67.二进制求和.js
@@ -10,8 +10,53 @@
  * @param {string} b
  * @return {string}
  */
-var addBinary = function(a, b) {
+var addBinary = function (a, b) {
+    function padLeft(str, len) {
+        if (str.length < len) {
+            while (str.length < len) {
+                str = "0" + str
+            }
+        }
+        return str
+    }
+    a.length>b.length?b=padLeft(b,a.length):a = padLeft(a,b.length)
+    a = a.split("").reverse()
+    a = a.map(function (item) {
+        return +item
+    })
+    b = b.split("").reverse()
+    b = b.map(function (item) {
+        return +item
+    })
+    let len = a.length
+    let carry  = 0
+    anw = []
+    a[len] = b[len] = 0
+    console.log(a,b);
     
+    for (let i = 0; i <= len; i++) {
+       
+            anw[i] = a[i]+b[i]+carry
+            
+            if (anw[i]==3){
+                anw[i] = 1
+                carry =1
+            }else if(anw[i] ==2){
+                anw[i] = 0
+                carry =1
+            }else{
+                carry = 0
+            }
+        
+
+    }
+    if (anw[anw.length-1] == 0) {
+        anw.pop()
+    }
+    return anw.reverse().join("")
 };
+
+
+
 // @lc code=end
 
